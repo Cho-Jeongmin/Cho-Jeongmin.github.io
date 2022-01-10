85ee51a4da4fc5935fc5547ea1bead66ef174768
Cho Jeongmin <97157930+Cho-Jeongmin@users.noreply.github.com>
2022-01-11 01:59:51 +0900

    nomadcoders #2.4

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
-const a = 5;
-const b = 2;    //const : 상수 선언. 변경 불가.(보통 상수로 선언)
-let myName = "Jeongmin";   //let : 변수 선언. 변경 가능.(필요할 때만 선언)
-var c = 3;  //옛날부터 있던 변수형. 변경가능. but, 쓰지 말것.
+const amIFat = true;    //boolean타입.
+const a = null;     //null은 아무 값도 없다는 뜻.
+let something;      //undefined. 정의되지 않음
 
-console.log(a + b);   //콘솔에 출력
-console.log(a * b);
-console.log(a / b);
-
-console.log("hello " + myName);
-
-myName = "Jamie";   //변수 업데이트
-
-console.log("Your new name is " + myName);
+console.log(amIFat);    //true
+console.log(a);     //null
+console.log(something); //undefined
