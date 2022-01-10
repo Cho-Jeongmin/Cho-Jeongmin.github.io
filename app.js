8be80fb02792eb46ef194c0b3c0fe2fa515042cc
Cho Jeongmin <97157930+Cho-Jeongmin@users.noreply.github.com>
2022-01-11 02:11:14 +0900

    nomadcoders #2.5 5:38

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-const amIFat = true;    //boolean타입.
-const a = null;     //null은 아무 값도 없다는 뜻.
-let something;      //undefined. 정의되지 않음
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sund";
 
-console.log(amIFat);    //true
-console.log(a);     //null
-console.log(something); //undefined
+// 참고 : 다중 커서는 Alt누른 상태로 클릭하면 가능
+
+//Array 생성
+const daysOfWeek = [mon, tue, wed, thu, fri, sat, sun];
+
+const nonsense = [1, 2, "hello", false, null, true, undefined, "Jamie"];
+
+console.log(daysOfWeek, nonsense);
