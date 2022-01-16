ed4203aaae46935cbc914a044892911b3574b8a8
Cho Jeongmin <97157930+Cho-Jeongmin@users.noreply.github.com>
2022-01-16 22:24:09 +0900

    nomadcoders #4.2
    submitEvent

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
-const loginForm = document.getElementById("login-form");
-const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
 
-function onLoginBtnClick() {
-    const userName = loginInput.value;
-    if(userName === ""){
-        alert("Please write your name.");
-    } else if(userName.length > 15){
-        alert("Your name is too long.");
-    } else {
-        console.log("hello", userName);
-    }
+function onLoginSubmit(event) {
+    event.preventDefault();//브라우저의 default행동을 막아줌(ex:새로고침)
+    console.log(loginInput.value);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
+//submitEvent는 엔터 또는 버튼을 누를 때 발생
+//브라우저는 handler를 호출할 때 해당 event object를 첫번째 argument로 넘겨줌.
