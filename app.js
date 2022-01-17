69ed80ab6057fc4332cfb6256a9970462a65181d
Cho Jeongmin <97157930+Cho-Jeongmin@users.noreply.github.com>
2022-01-18 00:37:49 +0900

    nomadcoders #4.6
    localStorage API

--- app.js
+++ app.js
@@ -2,13 +2,26 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden";//string만 담는 변수 이름 주로 대문자로 씀
+const USERNAME_KEY = "username";
 
-function onLoginSubmit(event) {
+function onLoginSubmit(event) { //제출 함수
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    const userName = loginInput.value;
-    greeting.innerText = `Hello ${userName}!`;//backtick 기호
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreetings(username) { //인삿말
+    greeting.innerText = `Hello ${username}!`;  //innerText를 추가하고
+    greeting.classList.remove(HIDDEN_CLASSNAME);//greeting을 보이게하기
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){ //이미 저장된 유저명이 없으면
+    loginForm.classList.remove(HIDDEN_CLASSNAME);   //폼을 보이게하고
+    loginForm.addEventListener("submit", onLoginSubmit);    //제출 진행
+} else {    //이미 저장된 유저명이 있으면
+    paintGreetings(savedUsername);  //인삿말
+}
