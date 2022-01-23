5dffb219e9ebc0fec0ae5367d4ce19498d4b0785
Cho Jeongmin <97157930+Cho-Jeongmin@users.noreply.github.com>
2022-01-24 04:42:32 +0900

    nomadcoders #8.2
    Fin.

---
+++ quotes.js
@@ -0,0 +1,51 @@
+
+const quotes = [
+    {
+        quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+        author: "Nelson Mandela",
+    },
+    {
+        quote: "The way to get started is to quit talking and begin doing.",
+        author: "Walt Disney",
+    },
+    {
+        quote: "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking.",
+        author: "Steve Jobs",
+    },
+    {
+        quote: "If life were predictable it would cease to be life, and be without flavor.",
+        author: "Eleanor Roosevelt",
+    },
+    {
+        quote: "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.",
+        author: "Oprah Winfrey",
+    },
+    {
+        quote: "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.",
+        author: "James Cameron",
+    },
+    {
+        quote: "Life is what happens when you're busy making other plans.",
+        author: "John Lennon",
+    },
+    {
+        quote: "Don't judge each day by the harvest you reap but by the seeds that you plant.",
+        author: "Robert Louis Stevenson",
+    },
+    {
+        quote: "Many of life's failures are people who did not realize how close they were to success when they gave up.",
+        author: "Thomas A. Edison",
+    },
+    {
+        quote: "You only live once, but if you do it right, once is enough.",
+        author: "Mae West",
+    },
+]
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+const todaysQuote = Math.floor(Math.random()*quotes.length);//0이상 length미만의 랜덤 넘버
+
+quote.innerText = quotes[todaysQuote].quote;
+author.innerText = `-${quotes[todaysQuote].author}-`;
